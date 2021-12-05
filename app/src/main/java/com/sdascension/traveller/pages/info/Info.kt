8399d456ad524d117fedd13c801e8fc35c7796ec
ASCksbl <ksbohorquezl@gmail.com>
2021-12-04 21:23:50 -0500

    Last changes and functionality for sprint 3

--- app/src/main/java/com/sdascension/traveller/pages/info/Info.kt
+++ app/src/main/java/com/sdascension/traveller/pages/info/Info.kt
@@ -3,6 +3,7 @@ package com.sdascension.traveller.pages.info
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
+// TODO: Still not in use (Not working)
 @Parcelize
 data class Info(
     val title: String,
